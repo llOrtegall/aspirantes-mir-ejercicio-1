c7672915b75ded81ca593dbb95cf19c147b5be8c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-02 13:51:10 -0500

    USSEeFFECT oK

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Prueba Técnica React
 Recuperar un hecho aleatorio de gatos con la primera API
+
+imagen ramdom: https://cataas.com/
+ -endpoint: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
 
+// const CAT_ENDPOINT = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
+const CAT_ENDPOINT_RAMDON = 'https://catfact.ninja/fact'
+
 export const App = () => {
   const [fact, setFac] = useState('aqui el app de gatitos')
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
+    fetch(CAT_ENDPOINT_RAMDON)
       .then(res => res.json())
       .then(data => setFac(data.fact))
   }, [])
