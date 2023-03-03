9fa43bfb77e2b173459f664f67bc15d876446796
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-03 14:30:18 -0500

    prueba terminada

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,34 @@ export const App = () => {
   const [fact, setFac] = useState()
   const [imageUrl, setImageUrl] = useState()
 
+  // EFECTO PARA RECUPERAR LA CITA
   useEffect(() => {
     fetch(CAT_ENDPOINT_RAMDON)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFac(fact)
-
-        const threefirstWord = fact.split(' ', 3)
-        console.log(threefirstWord)
-
-        fetch(`${CAT_ENDPOINT_WORD}/cat/says/${threefirstWord}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(`${CAT_ENDPOINT_WORD}${url}`)
-          })
       })
   }, [])
 
+  // PARA RECUPERAR LA IMAGEN
+  useEffect(() => {
+    if (!fact) return
+    const threefirstWord = fact.split(' ', 3)
+    console.log(threefirstWord)
+    fetch(`${CAT_ENDPOINT_WORD}/cat/says/${threefirstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
   return (
     <main>
       <h1>App De Gatitos</h1>
       <section>
         {fact && <p>{fact}</p>}
-        {imageUrl && <img src={imageUrl} alt='Image extracted using the firt worlds' />}
+        {imageUrl && <img src={`${CAT_ENDPOINT_WORD}${imageUrl}`} alt='Image extracted using the firt worlds' />}
       </section>
 
     </main>
