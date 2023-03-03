d2a933bfc327bba1fc63fe0f7957d5c5f35fb7cd
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-03 14:18:25 -0500

    prueba terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import { useEffect, useState } from 'react'
 
-// const CAT_ENDPOINT = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_ENDPOINT_WORD = 'https://cataas.com/'
 
 const CAT_ENDPOINT_RAMDON = 'https://catfact.ninja/fact'
 
 export const App = () => {
   const [fact, setFac] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RAMDON)
       .then(res => res.json())
-      .then(data => setFac(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFac(fact)
+
+        const threefirstWord = fact.split(' ', 3)
+        console.log(threefirstWord)
+
+        fetch(`${CAT_ENDPOINT_WORD}/cat/says/${threefirstWord}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(`${CAT_ENDPOINT_WORD}${url}`)
+          })
+      })
   }, [])
 
   return (
     <main>
       <h1>App De Renderizado</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt='Image extracted using the firt worlds' />}
     </main>
   )
 }
