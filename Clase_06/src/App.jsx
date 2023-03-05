50c97f234ea5fd441f4c393c1d67d7ad27c3383f
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-05 01:44:52 -0500

    proteger las turas

--- Clase_06/src/App.jsx
+++ Clase_06/src/App.jsx
@@ -1,7 +1,45 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import { Landing, Admin, Analytics, Dashboard, Home } from './Pages/index'
+
 export function App () {
   return (
-    <h1>
-      Hola Mundo
-    </h1>
+    <BrowserRouter>
+
+      <Navigation />
+
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Navigation () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/landing'>Landing</Link>
+        </li>
+        <li>
+          <Link to='/home'>Home</Link>
+        </li>
+        <li>
+          <Link to='/dashboard'>Dashboard</Link>
+        </li>
+        <li>
+          <Link to='/analytics'>Analytics</Link>
+        </li>
+        <li>
+          <Link to='/admin'>Admin</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
