442c26c3b5aba897f2349ff29498a02ed957ca4b
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-03 08:57:36 -0500

    renderizando avatar

--- Clase_04/src/App.jsx
+++ Clase_04/src/App.jsx
@@ -1,11 +1,19 @@
-function App() {
-
+export function App() {
   return (
+    <article>
+      <header>
+        <img alt="user avatar" src="https://unavatar.io/llOrTeGall" />
+        <div>
+          <strong>Ivan Ortega</strong>
+          <span>@llOrTeGall</span>
+        </div>
+      </header>
 
-    <div>
-      Hello World
-    </div>
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
   )
 }
-
-export default App
