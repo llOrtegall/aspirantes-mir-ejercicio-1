c120936ac1e2d7c3de7bc146ba8a200e968cb67c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-02 18:51:20 -0500

    primero pasos tic tac toe

--- Clase_05/src/App.jsx
+++ Clase_05/src/App.jsx
@@ -1,9 +1,29 @@
+const TURNOS = {
+  X: 'x',
+  O: 'o',
+}
+
+const TABLA = Array(9).fill(null)
+
 export function App() {
 
   return (
-    <div>
-      Hola
-    </div>
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          TABLA.map((_, i) => {
+            return (
+              <div className="cell" key={i}>
+                <span className="cell_content">
+                  {i}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
