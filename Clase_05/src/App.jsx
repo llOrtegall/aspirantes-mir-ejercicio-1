3bd909095de693161c88cc6cd7139e461784f96a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-03 19:33:22 -0500

    renderizar un ganador

--- Clase_05/src/App.jsx
+++ Clase_05/src/App.jsx
@@ -18,18 +18,55 @@ const Cuadrado = ({ children, isSelect, actualizaTabla, index }) => {
   )
 }
 
+const COMBINACION_GANADORA = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 export function App() {
 
   const [tabla, setTabla] = useState(Array(9).fill(null))
   const [turno, setTurno] = useState(TURNOS.X)
+  const [ganador, setGanador] = useState(null)
+
+  const verGanador = (tablaRevisada) => {
+    for (const combo of COMBINACION_GANADORA) {
+      const [a, b, c] = combo
+      if (
+        tablaRevisada[a] &&
+        tablaRevisada[a] === tablaRevisada[b] &&
+        tablaRevisada[a] === tablaRevisada[c]
+      ) {
+        return tablaRevisada[a]
+      }
+    }
+    return null
+  }
 
   const updateBoard = (index) => {
+    // TODO: si ya tiene algo
+    if (tabla[index] || ganador) return
+
+    // TODO: Actualizar el tablero
     const newTabla = [...tabla]
     newTabla[index] = turno // x u o
     setTabla(newTabla)
 
+    // TODO: Cambiar el turno
     const newTurno = turno === TURNOS.X ? TURNOS.O : TURNOS.X
     setTurno(newTurno)
+
+    //TODO: Revisamos si alguien gano
+    const newGanador = verGanador(newTabla)
+    if (newGanador) {
+      setGanador(newGanador)
+    }
   }
 
   return (
