e40c22849ce191c5319ca3ace5578f0cc02677f1
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-26 18:57:37 -0500

    primer estado

--- Clase_05/src/App.jsx
+++ Clase_05/src/App.jsx
@@ -1,24 +1,36 @@
+import { useState } from "react"
+
 const TURNOS = {
   X: 'x',
   O: 'o',
 }
 
-const TABLA = Array(9).fill(null)
+
+
+// eslint-disable-next-line react/prop-types
+const Cuadrado = ({ children, actualizaTabla, index }) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
 
 export function App() {
+
+  const [tabla, setTabla] = useState(Array(9).fill(null))
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {
-          TABLA.map((_, i) => {
+          tabla.map((_, index) => {
             return (
-              <div className="cell" key={i}>
-                <span className="cell_content">
-                  {i}
-                </span>
-              </div>
+              <Cuadrado key={index}
+                index={index}>
+                {index}
+              </Cuadrado>
             )
           })
         }
