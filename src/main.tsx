b84d0cad4edbd076e1e238d712468415564a5ee9
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-10 16:05:58 -0500

    validar eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const root = document.getElementById('root')
-
-if(root !== null) {
-  createRoot(root).render(<App />)
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
