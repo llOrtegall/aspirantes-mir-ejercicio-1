e0ff4afed6ebdf68a2c3cb3ab028ac989c10d69a
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-10 15:48:17 -0500

    desde 0 again

--- Clase_06/src/Components/ProtectdeRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-export const ProtectdeRoute = ({ isAllowed, children, redirectTo = '/landing' }) => {
-  if (!isAllowed) return <Navigate to={redirectTo} />
-
-  return children || <Outlet />
-}
